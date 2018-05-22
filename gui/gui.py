8db5c3c63c4bc48b2f95055d3952fecb839af6f8
Damian Lopez <dlopez2021@gmail.com>
2018-05-22 02:14:19 -0400

    gui template from online, use this to flesh out ssh-keygen GUI frame

--- gui/gui.py
+++ gui/gui.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python
 from Tkinter import *
 
-window = Tk()
-window.title("Damians GUI")
 
-l1 = Label(window, text="This is a label", font=("Arial Bold",50))
-l1.place(x=10,y=10)
+class MainApplication(Frame):
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
 
-btn1 = Button(window, text="Click Me", bg="black", fg="green", command=clicked)
-btn1.place(x=10,y=70)
+        #<create the rest of your GUI here>
 
-def clicked():
-    lb1.configure(text="You pressed the Button")
-
-window.geometry('500x400')
-
-
-
-
-
-window.mainloop()
+if __name__ == "__main__":
+    root = Tk()
+    MainApplication(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
